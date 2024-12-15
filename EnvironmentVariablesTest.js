04f7e0a3409c17068d8060bc04854d47c4f33226
Jonathan Washburn <jrwashburn@gmail.com>
2024-12-14 21:42:00 -0500

    test with real sends

--- EnvironmentVariablesTest.js
+++ EnvironmentVariablesTest.js
@@ -4,20 +4,19 @@ function setTestVariables() {
 
   if (TESTER === 'Wilyam') {
     SEND_EMAIL = false;
+
     // Specify your testing sheets/forms/etc. here:
     // Spreadsheets:
     //
-
-    // Wilyam test sheets
     AMBASSADOR_REGISTRY_SPREADSHEET_ID = '1AIMD61YKfk-JyP6Aia3UWke9pW15bPYTvWc1C46ofkU'; //"Ambassador Registry"
     AMBASSADORS_SCORES_SPREADSHEET_ID = '1RJzCo1FgGWkx0UCYhaY3SIOR0sl7uD6vCUFw55BX0iQ'; // "Ambassadors' Scores"
     AMBASSADORS_SUBMISSIONS_SPREADSHEET_ID = '1AIMD61YKfk-JyP6Aia3UWke9pW15bPYTvWc1C46ofkU'; // "Ambassador Submission Responses"
     EVALUATION_RESPONSES_SPREADSHEET_ID = '1RJzCo1FgGWkx0UCYhaY3SIOR0sl7uD6vCUFw55BX0iQ'; // "Ambassador Evaluations' Responses"
     FORM_RESPONSES_SHEET_NAME = 'Form Responses 14'; // Explicit name for 'Form Responses' sheet
     EVAL_FORM_RESPONSES_SHEET_NAME = 'Form Responses 2'; // Evaluation Form responses sheet
+
     // Google Forms
     //
-    // Wilyam test forms:
     SUBMISSION_FORM_ID = '1SV5rJbzPv6BQgDZkC_xgrauWgoKPcEmtk3aKY6f4ZC8'; // ID for Submission form
     EVALUATION_FORM_ID = '15UXnrpOOoZPO7XCP2TV7mwezewHY6UIsYAU_W_aoMwo'; // ID for Evaluation form
     SUBMISSION_FORM_URL = 'https://forms.gle/beZrwuP9Zs1HvUY49'; // Submission Form URL for mailing
@@ -25,7 +24,6 @@ function setTestVariables() {
 
     // Sponsor Email (for notifications when ambassadors are expelled)
     //
-    // Wilyam test sponsor
     SPONSOR_EMAIL = 'economicsilver@starmail.net'; // Sponsor's email
 
     // Sheet names
@@ -58,23 +56,21 @@ function setTestVariables() {
     COLOR_EXPELLED = '#d36a6a';
     COLOR_MISSED_SUBM_AND_EVAL = '#ea9999';
     COLOR_OLD_MISSED_SUBMISSION = '#ead1dc';
+
+    // Jonathan Test vars
+    //
+    //
   } else if (TESTER === 'Jonathan') {
-    SEND_EMAIL = false;
+    SEND_EMAIL = true;
 
     // Specify your testing sheets/forms/etc. here:
     // Spreadsheets:
-    //
-    //
-    // Jonathan test sheets
     AMBASSADOR_REGISTRY_SPREADSHEET_ID = '14aHm1EiK48RoclGYydI7OzcFcExYUr9tdQ6PlhjCbAg'; //"Ambassador Registry"
     AMBASSADORS_SCORES_SPREADSHEET_ID = '1lVUaCGCCbfD3l9e8MEfQVBKaljm7A5aKX7RJAsUrWfA'; // "Ambassadors' Scores"
     AMBASSADORS_SUBMISSIONS_SPREADSHEET_ID = '1lVUaCGCCbfD3l9e8MEfQVBKaljm7A5aKX7RJAsUrWfA'; // "Ambassador Submission Responses"
     EVALUATION_RESPONSES_SPREADSHEET_ID = '1lVUaCGCCbfD3l9e8MEfQVBKaljm7A5aKX7RJAsUrWfA'; // "Ambassador Evaluations' Responses"
 
     // Google Forms
-    //
-    //
-    // Jonathan test forms:
     SUBMISSION_FORM_ID = '13oDRgD2qjryfhv992ZS99zCTOHPXBxsqKAXijupHbfE'; // ID for Submission form
     EVALUATION_FORM_ID = '1EPrKCrg7NXfEje3Ps3aBZ3S_qs9oy3dZl6SbnF3Ek4U'; // ID for Evaluation form
     SUBMISSION_FORM_URL = 'https://forms.gle/FeZzfoD6pw1fJi9D8'; // Submission Form URL for mailing
@@ -83,9 +79,6 @@ function setTestVariables() {
     EVAL_FORM_RESPONSES_SHEET_NAME = 'Form Responses 4'; // Evaluation Form responses sheet
 
     // Sponsor Email (for notifications when ambassadors are expelled)
-    //
-    //
-    // Jonathan test sponsor
     SPONSOR_EMAIL = 'jonathan@jkw.fm'; // Sponsor's email
 
     // Sheet names
