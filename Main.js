2e72f8f0437205bab59e1f947cceae373837be1d
Jonathan Washburn <jrwashburn@gmail.com>
2025-09-06 21:34:07 -0400

    move onopen to main

--- Main.js
+++ Main.js
@@ -28,3 +28,22 @@ Logger.log(`Configuration loaded: ${CONFIG_NAME}`);
 if (typeof TESTER_EMAIL !== 'undefined') {
   Logger.log(`Tester email: ${TESTER_EMAIL}`);
 }
+
+//    On Open Menu
+function onOpen() {
+  const ui = SpreadsheetApp.getUi();
+  Logger.log('Initializing menu on spreadsheet open.');
+  ui.createMenu('Ambassador Program')
+    .addItem('Request Submissions', 'requestMonthlySubmissions') // Request Submissions
+    .addItem('Request Evaluations', 'requestEvaluationsModule') // Request Evaluations
+    .addItem('Compliance Audit', 'runComplianceAudit') // Process Scores and Penalties
+    .addItem('Notify Upcoming Peer Review', 'notifyUpcomingPeerReview') // Peer Review notifications
+    .addItem('Select CRT members', 'selectCRTMembers') // CRT
+    .addItem('🔧️Batch process scores', 'batchProcessEvaluationResponses') //Re-runs score responses
+    .addItem('🔧️Create/Sync Columns', 'syncRegistryColumnsToOverallScore') // creates Ambassador Status column in Overall score sheet; Syncs Ambassadors' Discord Handles and Ambassador Status columns between Registry and Overall score.
+    .addItem('🔧️Check Emails in Submission Form responses', 'validateEmailsInSubmissionForm') // Checks completance of emails in 'Your Email Address' field of Submission Form. Recommended to run before Evaluation Requests to avoid errors caused by users' typo.
+    .addItem('🔧️Delete Existing Triggers', 'deleteExistingTriggers') // Optional item
+    .addItem('🔧️Force Authorization', 'forceAuthorization') // Authorization trigger
+    .addToUi();
+  Logger.log('Menu initialized.');
+}
